7b3fd5e0fa25255b25aa45bca44cd862802f29ef
officezam <officezam@gmail.com>
2017-10-08 12:03:01 +0500

    Delete Extra files

--- core/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$config = array();
-
-$config['host'] 	= 	'localhost';
-$config['dbname']	=	'crypticcities';
-$config['user_name']=	'root';
-$config['password'] =	'';
--- core/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); 
-require_once ('db.php');
-require_once ('helper_func.php');
-
-$conn = connect($config);
-if ( ! $conn ) die('Couldn\'t connected to db!');
