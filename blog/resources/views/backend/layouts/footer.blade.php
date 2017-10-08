5a81a0c6bce98c516cb0f5e410bce35de74d9411
officezam <officezam@gmail.com>
2017-10-08 12:14:10 +0500

    Test

--- blog/resources/views/backend/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-
-<!-- footer content -->
-<footer>
-    <div class="pull-right">
-        Admin Template by <a href="#">Amir Shahzad</a>
-    </div>
-    <div class="clearfix"></div>
-</footer>
-<!-- /footer content -->
