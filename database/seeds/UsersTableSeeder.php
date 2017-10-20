40932c5a56b5db1519827ac743166dee0101ed06
officezam <officezam@gmail.com>
2017-10-20 23:40:01 +0500

    Seeder Write for users Table

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Ghazanfar Rehman',
+            'type' => 'admin',
+            'email' => 'officezam@gmail.com',
+            'address' => 'Al-madin Electric Store Bata Choke Gulgasht colony Multan',
+            'phone' => '03007272332',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
