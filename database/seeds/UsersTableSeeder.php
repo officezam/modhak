008d2b4486f16ae94ad5b63dc8ec198963569485
officezam <officezam@gmail.com>
2017-10-20 23:59:18 +0500

    Add three

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,12 +13,30 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Ghazanfar Rehman',
+            [
+            'name' => 'Amir Shahzad',
             'type' => 'admin',
             'email' => 'officezam@gmail.com',
             'address' => 'Al-madin Electric Store Bata Choke Gulgasht colony Multan',
             'phone' => '03007272332',
             'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Ghazanfar Rehman',
+                'type' => 'admin',
+                'email' => 'grehman@gmail.com',
+                'address' => 'Canada',
+                'phone' => '+17802456176',
+                'password' => bcrypt('pass2word'),
+            ],
+            [
+                'name' => 'Ghazanfar Rehman',
+                'type' => 'admin',
+                'email' => 'moon3204@gmail.com',
+                'address' => 'Canada',
+                'phone' => '17802456176',
+                'password' => bcrypt('pass2word'),
+            ]
         ]);
     }
 }
