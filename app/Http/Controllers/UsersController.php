1e72a3f2d341b526bb468b0d4bf0dd6d51799337
officezam <officezam@gmail.com>
2017-10-22 01:24:11 +0500

    check ENV Object

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -63,6 +63,8 @@ class UsersController extends Controller
     }
     public function register(Request $request)
     {
+        $environment = $app->environment();
+        dd($environment);
         $this->validator($request->all())->validate();
 
         event( $this->create($request->all()));
