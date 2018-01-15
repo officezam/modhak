52ad03e123bb42fc20fc5a7ec35a36cd3e82a9c4
officezam <officezam@gmail.com>
2018-01-15 23:59:45 +0500

    Added leadsdetail

--- app/Leadsdetail.php
+++ app/Leadsdetail.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Leadsdetail extends Model
 {
-	protected $table = 'Leadsdetail';
+	protected $table = 'leadsdetail';
 	protected $fillable = ['leads_id', 'question', 'answer', 'question_no', 'static_reply', 'audio'];
 
 }
