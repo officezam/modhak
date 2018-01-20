6b2055dbd80636b16ad35497e0971fc856fe14a6
officezam <officezam@gmail.com>
2018-01-20 16:48:45 +0500

    Added files

--- app/Http/Controllers/SmsSendTwilioController.php
+++ app/Http/Controllers/SmsSendTwilioController.php
@@ -131,7 +131,7 @@ class SmsSendTwilioController extends Controller
 					}
 
 				}
-				Members::where('phone' ,'=',$from_number)->update(['funnel_type' => 'test', 'question_id' => $question_id,'last_answer' => $last_answer ]);
+				Members::where('phone' ,'=',$from_number)->update(['member_funnel' => 'test', 'question_id' => $question_id,'last_answer' => $last_answer ]);
 			}else
 			{
 				if($body == 'unsub' || $body == 'unsubscribe'){
